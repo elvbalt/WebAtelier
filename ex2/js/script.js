6863b760b4bdba1162d78a31d8ee1ea2fbad2e32
Elvira <elvira.baltasar.jimenez@usi.ch>
2023-10-03 14:49:52 +0200

    first

--- ex2/js/script.js
+++ ex2/js/script.js
@@ -1,7 +1,7 @@
 /**
  * Web Atelier 2023 2 - JavaScript
  *
- * Student: __STUDENT NAME__
+ * Student: __Elvira Baltasar__
  *
  * Write your solution by completing the following functions
  *
@@ -20,7 +20,38 @@
  * If the input value is not a number, it should be converted to a number before processing.
  */
 function format_thousands(n) {
-}
+    if (isNaN(n) || n === undefined){
+        return "???";
+    }
+    if(n < 1000 && n > -1000){
+        return String(n);
+    } else{
+
+        const num = n.toString();
+  
+        let parts = num.split('.');
+
+        let integerPart = parts[0];
+        let decPart = parts[1];
+      
+        //https://stackoverflow.com/questions/2254185/regular-expression-for-formatting-numbers-in-javascript
+        //https://blog.devgenius.io/how-to-format-a-number-with-commas-as-thousands-digit-separators-in-javascript-ce6ff8475192
+        // the /\B(?=(\d{3}) looks for groups of 3 digits before the decimal place
+        //the \B hace q no se añada ' al principio
+        //?= nos asegura q solo se añade ' si a la izq de los 3 digitos hay otro
+
+        const Integer = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, "'");
+      
+        let formattedn = Integer;
+        if (decPart !== undefined) {
+          formattedn += '.' + decPart;
+        }
+      
+        return formattedn;
+
+    }
+    }
+
 
 
 
@@ -32,6 +63,22 @@ function format_thousands(n) {
 * with the input scalar value `c`.
 */
 function scalar_product(a, c) {
+    if (isNaN(c) || c === undefined ||! Array.isArray(a)){
+        return undefined;
+    }
+    if (c === 1){
+        return a;
+    }
+    if(c === 0){
+        return new Array(a.length).fill(0);
+    }
+    let i = 0;
+    let res = new Array(a.length);
+    for (i = 0; i < a.length; i++){
+        res[i] = a[i] * c;
+    }
+
+    return res;
 }
 
 
@@ -42,6 +89,17 @@ function scalar_product(a, c) {
  * of elements of its input arrays `a`, `b` in the same position.
  */
 function inner_product(a, b) {
+    if (! Array.isArray(a) || ! Array.isArray(b) || a.length !== b.length){
+        return undefined;
+    }
+
+    let i = 0;
+    let res = 0;
+    for (i = 0; i < a.length; i++){
+        res = a[i] * b[i] + res;
+    }
+
+    return res;
 }
 
 
@@ -55,6 +113,53 @@ function inner_product(a, b) {
  *   The inner array contains the values found separated by commas on each line.
  */
 function parseCSV(csv, drop_header = true, only_header = false) {
+    if (drop_header && only_header){
+        return [];
+    }
+    
+    if (typeof csv !== 'string' || Array.isArray(csv)){
+        return undefined;
+    }
+
+    
+    
+    let a = csv.split('\n');
+
+    if(a.length > 0 && a[a.length-1] === ''){
+        a.pop();
+    }
+
+    let res = [];
+
+    if (only_header) { 
+        return a[0].split(',');
+    }
+
+    for (let i = 0; i<a.length; i++){
+        const value = a[i].split(',');
+        res.push(value);
+    }
+
+    if (drop_header){
+        res.shift();
+    }
+    return res;
+
+
+    /*if (arr[arr.length-1].length === 0){
+        arr.pop();
+    }
+    if(drop_header){
+        for (let i = 0; i < arr.length; i++){
+            res.push(arr[i+1].split(","));
+        }
+    }else if (only_header){
+        return arr[0].split(",");
+    }else{
+        for (let i = 0; i < arr.length; i++){
+            res.push(arr[i].split(","));
+        }
+    }*/
 }
 
 
@@ -63,6 +168,26 @@ function parseCSV(csv, drop_header = true, only_header = false) {
  * @return {String[][]} - the content of the CSV, keeping only the lines which do not include empty entries.
  */
 function clean(csv_array) {
+    if (csv_array === undefined || !Array.isArray(csv_array)){
+        return undefined;
+    }
+
+    return csv_array.filter(line => line.every(value => value !== ''));
+    //return csv_array.filter(line => !line.includes("")); //some(value => value.trim() === ''));
+
+    //return csv_array.filter(line => !line.contais(" "));
+
+    /*let clean_array = csv_array.filter((arr) => {
+        let dejar = true;
+        for (let i= 0; i != arr.length; i++){
+            if(!arr[i]){
+                dejar = false;
+            }
+        }
+        return dejar;
+    })*/
+
+    //return clean_array;
 }
 
 /**
@@ -72,6 +197,51 @@ function clean(csv_array) {
  * @return {Integer[]} - an array with the content of the selected column converted to integer.
  */
 function extractIntegers(csv_array, column) {
+    /*if (csv_array === undefined || !Array.isArray(csv_array) || csv_array.length === 0){ // || !Array.isArray(csv_array)
+        return undefined;
+    }
+    if (column > csv_array[0].length -1 || column < 0){
+        return undefined;
+    }
+
+    let integers = [];
+
+    for (let i = 0; i < csv_array.length; i++){
+        if (isNaN(csv_array[i][column])){
+            integers.push(NaN);
+        }else{
+            integers.push(parseInt(csv_array[i][column], 10));
+        }
+    }
+    return integers;*/
+
+    if (!Array.isArray(csv_array) || column === undefined || column < 0 || column >= csv_array[0].length) {
+        return undefined;
+    }
+
+    if (csv_array.length === 0){
+        return [];
+    }
+    let integers = [];
+
+    integers = csv_array.map(line => {
+        const value = parseInt(line[column], 10);
+        return isNaN(value) ? NaN : value; });
+
+    return integers;
+    /*for (let i = 0; i < csv_array.length; i++) {
+        
+        let value = parseInt(csv_array[i][column]);
+
+        
+        if (isNaN(value)) {
+            integers.push(NaN);
+        } else {
+            integers.push(value);
+        }
+    }
+
+    return integers;*/
 }
 
 
@@ -134,6 +304,29 @@ function str2gps(dms) {
  * The function throws an error if called again after it reaches the last element.
  */
 function iterator(a) {
+    if (!Array.isArray(a)){
+        return undefined;
+    }
+
+    let i = 0;
+
+    function next(shift) {
+        if (Array.isArray(shift)) {
+          a = shift;
+          i = 0;
+          return next;
+        } else if (typeof shift === 'number') {
+          i += shift;
+          return i;
+        } else if (i < a.length) {
+          return a[i++];
+        } else {
+          throw new Error('No more values left in the array');
+        }
+      }
+    
+
+  return next;
 }
 
 
@@ -196,10 +389,16 @@ function initMap() {
 
     let dom_range = document.querySelector("#range-population");
 
+    createMap([0,0]);
+
+    let cities = parseCSV(cities_csv);
+    clean(cities);
+    extractIntegers(cities);
 
     dom_range.min = 0; //TODO replace with the minimum population value
-    dom_range.max = 0; //TODO replace with the maximum population value
+    dom_range.max = 37732000; //TODO replace with the maximum population value
     dom_range.value = (dom_range.min + dom_range.max) / 2;
+
 
 
     dom_range.addEventListener("input", e=>{
@@ -208,6 +407,8 @@ function initMap() {
 
     })
 
+    
+
     /**
      * Display the cities on the map when the input[type="range"] HTML element changes
      * It should only include the cities with population > minpop
@@ -215,7 +416,10 @@ function initMap() {
      *
      */
     function displayCities(minpop) {
-
+        removeMarkers();
+        //for de population y con el indice q cumpla 
+        //la condicion saco de cities la long y lat
+        createMarker(lat, long);
 
     }
 
